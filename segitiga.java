ab10acbd846a4d91cfe5f70f12a4ac748f7f4621
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2024-02-01 11:15:11 +0700

    eight

---
+++ segitiga.java
@@ -0,0 +1,8 @@
+public class segitiga {
+    public static void main(String[] args) {
+        double a = 12;
+        double b = 10;
+        double c = Math.pow(a, 2) + Math.pow(b, 2);
+        System.out.println("Hasilnya adalah : " + c);
+    }
+}
